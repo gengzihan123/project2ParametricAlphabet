688aee213fb7159d01a092d4d8df8dfe7c28c036
gengzihan123 <61759113+gengzihan123@users.noreply.github.com>
2020-05-14 19:03:43 +1200

    Add files via upload

---
+++ draw_letters.js
@@ -0,0 +1,41 @@
+const colorFront1  = "#199cff";
+const colorFront2  = "#59ccff";
+const colorStroke  = "#233f11";
+
+/*
+ * Draw the letter given the letterData
+ *
+ * Letters should always be drawn with the
+ * following bounding box guideline:
+ * from (0,0) to (100, 200)
+ */
+function drawLetter(letterData) {
+  // color/stroke setup
+  stroke(colorStroke);
+  strokeWeight(4);
+
+  // determine parameters for second circle
+  let size2 = letterData["size"];
+  let pos2x = 50  + letterData["offsetx"];
+  let pos2y = 150 + letterData["offsety"];
+
+  // draw two circles
+  fill(colorFront1);
+  ellipse(50, 150, 75, 75);
+  fill(colorFront2);
+  ellipse(pos2x, pos2y, size2, size2);
+}
+
+function interpolate_letter(percent, oldObj, newObj) {
+  let new_letter = {};
+  new_letter["size"]    = map(percent, 0, 100, oldObj["size"], newObj["size"]);
+  new_letter["offsetx"] = map(percent, 0, 100, oldObj["offsetx"], newObj["offsetx"]);
+  new_letter["offsety"] = map(percent, 0, 100, oldObj["offsety"], newObj["offsety"]);
+  return new_letter;
+}
+
+var swapWords = [
+  "ABBAABBA",
+  "CAB?CAB?",
+  "BAAAAAAA"
+]
---
+++ sketch.js
@@ -0,0 +1,85 @@
+const canvasWidth = 960;
+const canvasHeight = 500;
+
+/* 
+ * my three variable per letter are:
+ *
+   size: radius of the second circle (in pixels)
+   offsetx: x offset (in pixels) of the second circle
+            relative to the first one
+   offsety: y offset (in pixels) of the second circle
+            relative to the first one
+ *
+ */
+
+const letterA = {
+  "size": 80,
+  "offsetx": 0,
+  "offsety": 35
+}
+
+const letterB = {
+  "size": 150,
+  "offsetx": 0,
+  "offsety": -145
+}
+
+const letterC = {
+  "size": 100,
+  "offsetx": 30,
+  "offsety": 0
+}
+
+const colorFront1  = "#199cff";
+const colorFront2  = "#59ccff";
+const colorBack    = "#e3eded";
+const colorStroke  = "#233f11";
+
+function setup () {
+  // create the drawing canvas, save the canvas element
+  main_canvas = createCanvas(canvasWidth, canvasHeight);
+  main_canvas.parent('canvasContainer');
+
+  // color/stroke setup
+  stroke(colorStroke);
+  strokeWeight(4);
+
+  // with no animation, redrawing the screen is not necessary
+  noLoop();
+}
+
+function drawLetter(posx, posy, letterData) {
+  // determine parameters for second circle
+  let size2 = letterData["size"];
+  let pos2x = posx + letterData["offsetx"];
+  let pos2y = posy + letterData["offsety"];
+
+  // draw two circles
+  fill(colorFront1);
+  ellipse(posx, posy, 150, 150);
+  fill(colorFront2);
+  ellipse(pos2x, pos2y, size2, size2);
+}
+
+function draw () {
+  // clear screen
+  background(colorBack);
+
+  // compute the center of the canvas
+  let center_x = canvasWidth / 2;  
+  let center_y = canvasHeight / 2;
+
+  // draw the letters A, B, C from saved data
+  drawLetter(center_x - 250, center_y, letterA);
+  drawLetter(center_x      , center_y, letterB);
+  drawLetter(center_x + 250, center_y, letterC);
+}
+
+function keyTyped() {
+  if (key == '!') {
+    saveBlocksImages();
+  }
+  else if (key == '@') {
+    saveBlocksImages(true);
+  }
+}
