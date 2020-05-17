b72b03c2011a33ab881337284208adac88082c46
gengzihan123 <61759113+gengzihan123@users.noreply.github.com>
2020-05-17 18:42:32 +1200

    make A to O

--- draw_letters.js
+++ draw_letters.js
@@ -1,5 +1,9 @@
-const colorFront1  = "#199cff";
-const colorFront2  = "#59ccff";
+const colorFront1  = "#f71735";//big, main
+const colorFront2  = "#5aa9e6";//small
+const colorFront3  = "#fdfffc";//small
+const colorFront4  = "#273043";//small
+
+
 const colorStroke  = "#233f11";
 
 /*
@@ -15,15 +19,48 @@ function drawLetter(letterData) {
   strokeWeight(4);
 
   // determine parameters for second circle
-  let size2 = letterData["size"];
-  let pos2x = 50  + letterData["offsetx"];
-  let pos2y = 150 + letterData["offsety"];
+  let posx1 = 50+letterData["x1"];
+  let posy1 = 100+letterData["y1"];
+  let posw1 = letterData["w1"];
+  let posh1 = letterData["h1"];
+
+  let posx2 = 50+letterData["x2"];
+  let posy2 = 100+letterData["y2"];
+  let posw2 = letterData["w2"];
+  let posh2 = letterData["h2"];
+
+  let posx3 = 50+letterData["x3"];
+  let posy3 = 100+letterData["y3"];
+  let posw3 = letterData["w3"];
+  let posh3 = letterData["h3"];
+
+
+  let posx4 = 50+letterData["x4"];
+  let posy4 = 100+letterData["y4"];
+  let posw4 = letterData["w4"];
+  let posh4 = letterData["h4"];
 
   // draw two circles
+  push()
+  rectMode(CENTER)
+  fill(colorFront4)
+  rect(50, 100, 100, 200, 20)
+
   fill(colorFront1);
-  ellipse(50, 150, 75, 75);
+  rect(posx1, posy1, posw1, posh1, 15)
+
   fill(colorFront2);
-  ellipse(pos2x, pos2y, size2, size2);
+  rect(posx2, posy2, posw2, posh2, 15)
+
+  fill(colorFront3);
+  rect(posx3, posy3, posw3, posh3, 15)
+  rect(posx4, posy4, posw4, posh4, 15)
+  pop()
+
+  //   fill(colorFront1);
+  // ellipse(50, 150, 75, 75);
+  // fill(colorFront2);
+  // ellipse(pos2x, pos2y, size2, size2);
 }
 
 function interpolate_letter(percent, oldObj, newObj) {
